cb6fe9929dec6cecc993a24a2e26a32c07de0106
sergigraum <sergi@unmanned.life>
2021-07-16 16:45:19 +0200

    Remove auto node registration

--- image_publisher/src/image_publisher.cpp
+++ image_publisher/src/image_publisher.cpp
@@ -268,10 +268,3 @@ void ImagePublisher::onInit()
 }
 
 }  // namespace image_publisher
-
-//#include "rclcpp_components/register_node_macro.hpp"
-
-// Register the component with class_loader.
-// This acts as a sort of entry point, allowing the component to be discoverable when its library
-// is being loaded into a running process.
-//RCLCPP_COMPONENTS_REGISTER_NODE(image_publisher::ImagePublisher)
